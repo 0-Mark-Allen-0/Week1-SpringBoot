bc56953c1ba229ca1ff2d3f6385bf377a7c8ff9a
Vincent <m.a.vincent@accenture.com>
2025-06-12 15:04:09 +0530

    Rectified issues with test failures v3.6

--- src/test/java/com/example/AccWeek1/AccWeek1ApplicationTests.java
+++ src/test/java/com/example/AccWeek1/AccWeek1ApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.AccWeek1;
 import com.example.AccWeek1.kafka.NotificationProducer;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -10,7 +11,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 @SpringBootTest
 @ActiveProfiles("test")
 class AccWeek1ApplicationTests {
-	@Disabled("Fails in CI due to Kafka")
+	@DisabledIfEnvironmentVariable(named = "CI", matches = "true")
 	@Test
 	void contextLoads() {
 	}

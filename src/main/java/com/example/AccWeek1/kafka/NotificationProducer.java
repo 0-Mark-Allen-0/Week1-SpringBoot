6ec9eea2858f2425bef798611c33a3b362e20d4d
Vincent <m.a.vincent@accenture.com>
2025-06-12 14:41:18 +0530

    Rectified issues with test failures v3.0

--- src/main/java/com/example/AccWeek1/kafka/NotificationProducer.java
+++ src/main/java/com/example/AccWeek1/kafka/NotificationProducer.java
@@ -1,10 +1,12 @@
 package com.example.AccWeek1.kafka;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 //This is a SERVICE for notification production
 @Service
+@ConditionalOnProperty(name = "kafka.enabled", havingValue = "true", matchIfMissing = true)
 public class NotificationProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
--- src/test/java/com/example/AccWeek1/AccWeek1ApplicationTests.java
+++ src/test/java/com/example/AccWeek1/AccWeek1ApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.AccWeek1;
 
+import com.example.AccWeek1.kafka.NotificationProducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AccWeek1ApplicationTests {
 
 	@Test
